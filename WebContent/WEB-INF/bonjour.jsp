03bebec7eb33fdfa804c963a728de87f300804a9
G; Vieira <sandalfon@gmail.com>
2016-10-18 13:24:31 +0200

    upload form 1

--- WebContent/WEB-INF/bonjour.jsp
+++ WebContent/WEB-INF/bonjour.jsp
@@ -1,3 +1,5 @@
+<%@ page pageEncoding="UTF-8" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,21 +7,19 @@
 <title>Test</title>
 </head>
 <body>
-	<c:if test="${!empty form.resultat }">
-		<p>
-			<c:out value="${form.resultat}" />
-		</p>
-	</c:if>
-	<form method="post" action="bonjour">
-		<p>
-			<label for="login">Login: </label> <input type="text" id="login"
-				name="login" />
-		</p>
-		<p>
-			<label for="mdp">Mot de passe: </label> <input type="password" id="mdp"
-				name="mdp" />
-		</p>
-		<input type="submit">
-	</form>
+    <c:if test="${ !empty fichier }"><p><c:out value="Le fichier ${ fichier } (${ description }) a été uploadé !" /></p></c:if>
+    <form method="post" action="bonjour" enctype="multipart/form-data">
+        <p>
+            <label for="description">Description du fichier : </label>
+            <input type="text" name="description" id="description" />
+        </p>
+        <p>
+            <label for="fichier">Fichier à envoyer : </label>
+            <input type="file" name="fichier" id="fichier" />
+        </p>
+        
+        <input type="submit" />
+    </form>
+    
 </body>
 </html>
--- src/com/octest/servlets/Test.java
+++ src/com/octest/servlets/Test.java
@@ -1,41 +1,97 @@
 package com.octest.servlets;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
-import com.octest.beans.Auteur;
 import com.octest.forms.ConnectionForm;
 
-
+/**
+ * Servlet implementation class Test
+ */
 @WebServlet("/Test")
 public class Test extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-
+    public static final int TAILLE_TAMPON = 10240;
+    public static final String CHEMIN_FICHIERS = "F:/Perso/workspace/OC_JavaEE/files/upload/"; // A changer
+    
+       
     public Test() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        // On récupère le champ description comme d'habitude
+        String description = request.getParameter("description");
+        request.setAttribute("description", description );
 
-	        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
-	}
+        // On récupère le champ du fichier
+        Part part = request.getPart("fichier");
+            
+        // On vérifie qu'on a bien reçu un fichier
+        String nomFichier = getNomFichier(part);
 
+        // Si on a bien un fichier
+        if (nomFichier != null && !nomFichier.isEmpty()) {
+            String nomChamp = part.getName();
+            // Corrige un bug du fonctionnement d'Internet Explorer
+             nomFichier = nomFichier.substring(nomFichier.lastIndexOf('/') + 1)
+                    .substring(nomFichier.lastIndexOf('\\') + 1);
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ConnectionForm form = new ConnectionForm();
-		form.verifId(request);
-		request.setAttribute("form", form);
-		  this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
-		
-		
-	}
+            // On écrit définitivement le fichier sur le disque
+            ecrireFichier(part, nomFichier, CHEMIN_FICHIERS);
 
+            request.setAttribute(nomChamp, nomFichier);
+        }
+
+        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
+    }
+
+    private void ecrireFichier( Part part, String nomFichier, String chemin ) throws IOException {
+        BufferedInputStream entree = null;
+        BufferedOutputStream sortie = null;
+        try {
+            entree = new BufferedInputStream(part.getInputStream(), TAILLE_TAMPON);
+            sortie = new BufferedOutputStream(new FileOutputStream(new File(chemin + nomFichier)), TAILLE_TAMPON);
+
+            byte[] tampon = new byte[TAILLE_TAMPON];
+            int longueur;
+            while ((longueur = entree.read(tampon)) > 0) {
+                sortie.write(tampon, 0, longueur);
+            }
+        } finally {
+            try {
+                sortie.close();
+            } catch (IOException ignore) {
+            }
+            try {
+                entree.close();
+            } catch (IOException ignore) {
+            }
+        }
+    }
+    
+    private static String getNomFichier( Part part ) {
+        for ( String contentDisposition : part.getHeader( "content-disposition" ).split( ";" ) ) {
+            if ( contentDisposition.trim().startsWith( "filename" ) ) {
+                return contentDisposition.substring( contentDisposition.indexOf( '=' ) + 1 ).trim().replace( "\"", "" );
+            }
+        }
+        return null;
+    }   
 }
